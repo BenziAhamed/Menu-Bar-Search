ac77d01c6b4ef30c4acd02e075cc2068ffd827ad
Benzi Ahamed <benzii@fb.com>
2021-12-30 01:51:20 +0000

    1.8 fixed universal builds for arm64 M1+ and x86_64 Intel

---
+++ menu/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "menu",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "menu",
+            dependencies: ["SwiftProtobuf"]),
+        .testTarget(
+            name: "menuTests",
+            dependencies: ["menu"]),
+    ]
+)
---
+++ menu/README.md
@@ -0,0 +1,8 @@
+# menu
+
+Search through menu options for front-most application - an Alfred Workflow
+
+For more info: https://github.com/BenziAhamed/Menu-Bar-Search
+
+## Building
+`xcrun swift build -c release --arch arm64 --arch x86_64`
